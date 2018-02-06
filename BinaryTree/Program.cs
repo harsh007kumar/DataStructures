90259da5ec993009e35342c1ff0bbc425235d276
Harsh via Mac <Harsh007kumar@gmail.com>
2018-02-07 02:58:52 +0530

    Initial Commit

---
+++ BinaryTree/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BinaryTree
+{
+    class Tree
+    {
+        public int Data { get; set; }
+        public Tree Left;
+        public Tree Right;
+
+        public Tree(int no)
+        {
+            Data = no;
+            Left = Right = null;
+        }
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
