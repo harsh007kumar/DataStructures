3c3638352b3f6e1b19357ed6983964587fe0ce49
Harsh Kumar <harsh007kumar@gmail.com>
2020-05-18 01:36:17 +0530

    added Hashing_using_SeperateChaining
    added Hashing_using_SeperateChaining
    Insert, Search. Delete and Display functions.

---
+++ Hashing/CommonFunctions.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hashing
+{
+    /// <summary>
+    /// One of the Collision Handling Technique utilized in Hashing
+    /// </summary>
+    public static class OpenAddressing
+    {
+        /// <summary>
+        /// Hi(x) = (Hash(x) + i) % HashtableSize
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="iteration"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static int LinearProbling(int key, int iteration, int size) => (CommonUtility.HashFunc1(key, size) + iteration) % size;
+
+        /// <summary>
+        /// Hi(x) = (Hash(x) + i raised to power 2) % HashtableSize
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="iteration"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static int QuadraticProbling(int key, int iteration, int size) => (CommonUtility.HashFunc1(key, size) + (int)Math.Pow(iteration, 2)) % size;
+
+        /// <summary>
+        /// Hi(x) = (Hash1(x) + i * Hash2(x)) % HashtableSize
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="iteration"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static int DoubleHashing(int key, int iteration, int size) => (CommonUtility.HashFunc1(key, size) + iteration * CommonUtility.HashFunc2(key, size)) % size;
+    }
+
+    public static class CommonUtility
+    {
+        public static int HashFunc1(int key, int size) => key % size;
+
+        public static int HashFunc2(int key, int size)
+        {
+            int primeNo = FindPrime(size);
+            return (primeNo - key % primeNo);
+        }
+
+        public static int FindPrime(int num)
+        {
+            if (num < 2)
+                return num;
+
+            int prime = 1;
+            // loop to iterate thru all integers smaller than num to find larget prime no
+            for (prime = num - 1; prime > 1; prime--)
+            {
+                bool isPrime = true;
+                // loop to check given number is prime or not
+                for (int i = (int)Math.Sqrt(prime); i > 1; i--)
+                {
+                    if (prime % i == 0)
+                    { isPrime = false; break; }
+                }
+                // break outer loop if prime found
+                if (isPrime)
+                    break;
+            }
+            return prime;
+        }
+    }
+}
---
+++ Hashing/Hashing_using_SeperateChaining.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hashing
+{
+    //GFG Link https://www.geeksforgeeks.org/hashing-set-2-separate-chaining/
+
+    /// <summary>
+    /// Hashing is improvement for Direct access table, below implementation utilizes Seperate Chaining method for collision handling
+    /// Time Complexity O( 1 + LoadFactor) for Search & Delete and O(1) for Insert
+    /// LoadFactor = N/M, where N = no of key to be inserteed in HashTable & M = total no of slots in HashTable
+    /// </summary>
+    class Hashing_using_SeperateChaining
+    {
+        private List<int>[] _ArrayOfGenericList;
+        public int Capacity { get; set; }
+        //Constructor
+        public Hashing_using_SeperateChaining(int size)
+        {
+            Capacity = size;
+            _ArrayOfGenericList = new List<int>[Capacity];
+        }
+
+        public void Insert(int key)                                             //Time Complexcity O(1)
+        {
+            int index = CommonUtility.HashFunc1(key, Capacity);
+            if (_ArrayOfGenericList[index] == null)                             //Initialize GenericList at each slot in HashTable
+                _ArrayOfGenericList[index] = new List<int>();
+            _ArrayOfGenericList[index].Add(key);
+            Console.WriteLine($"Insert SUCCESS\t|| Key : {key} inserted at index : {index}");
+        }
+
+        public void SearchKey(int key)                                          //Time Complexcity O(1+loadFactor)
+        {
+            int index = CommonUtility.HashFunc1(key, Capacity);
+            if (CheckValidIndex(index) && _ArrayOfGenericList[index]!=null)
+            {
+                foreach (int value in _ArrayOfGenericList[index])
+                    if (value == key)
+                    {
+                        Console.WriteLine($"Key : {key} found at index : {index}");
+                        break;
+                    }
+            }
+            else
+                Console.WriteLine($"Search FAIL\t|| Key : {key} not present in HashTable");
+        }
+
+        public void Delete(int key)                                             //Time Complexcity O(1+loadFactor)
+        {
+            int index = CommonUtility.HashFunc1(key, Capacity);
+            if (CheckValidIndex(index) && _ArrayOfGenericList[index] != null)
+            {
+                foreach (int value in _ArrayOfGenericList[index])
+                    if (value == key)
+                    {
+                        Console.WriteLine($"Key : {key} deleted from associated list at index : {index}");
+                        _ArrayOfGenericList[index].Remove(key);
+                        break;
+                    }
+            }
+            else
+                Console.WriteLine($"Delete FAIL\t|| Key : {key} not present in HashTable");
+        }
+
+        public void DisplayAll()
+        {
+            Console.WriteLine("\tDisplaying all keys present in HashTable");
+            for (int index = 0; index < Capacity; index++)
+            {
+                Console.Write($"Index : {index} ");
+                if (_ArrayOfGenericList[index] != null)
+                    foreach (int value in _ArrayOfGenericList[index])
+                        Console.Write($"-->{value}");
+                Console.WriteLine();
+            }
+        }
+        
+        private bool CheckValidIndex(int index) => (index >= 0 && index < Capacity)? true : false;
+    }
+}
