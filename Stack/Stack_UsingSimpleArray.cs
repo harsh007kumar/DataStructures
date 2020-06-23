24c8e4826bfade18dcd202f7df6e1645d010aa6a
Harsh Kumar <harsh007kumar@gmail.com>
2020-06-23 22:17:08 +0530

    Implemented STACK Simple array based implementation
    Implemented STACK Simple array based implementation

---
+++ Stack/Stack_UsingSimpleArray.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    /// <summary>
+    /// Simple array based implementation [UsingSimpleArray]
+    /// Karumanchi, Narasimha.Data Structures and Algorithms Made Easy: Data Structures and Algorithmic Puzzles(p. 166). Kindle Edition.
+    /// </summary>
+    public class Stack
+    {
+        protected int[] arr;
+        protected int Top { get; set; }
+        protected int Capacity { get; set; }
+
+        public Stack(int size)
+        {
+            arr = new int[size];
+            Capacity = size;
+            Top = -1;               // Indicates Stack Empty
+        }
+
+        /// <summary>
+        /// Inserts new element on the top of stack(if not Full).
+        /// </summary>
+        /// <param name="data"></param>
+        public void Push(int data)
+        {
+            if (Top == Capacity - 1) throw new Exception("STACK OVERFLOW EXCEPTION : Stack is Full");
+            arr[++Top] = data;      // Increment the Top by 1 and insert data at new Top
+        }
+
+        /// <summary>
+        /// Removes and returns the last inserted element from the stack.
+        /// </summary>
+        /// <returns></returns>
+        public int Pop() => (Top > 0) ? arr[Top--] : throw new Exception("STACK UNDERFLOW EXCEPTION : Stack is Empty");
+
+        public int Peek() => (Top > 0) ? arr[Top] : throw new Exception("STACK UNDERFLOW EXCEPTION : Stack is Empty");
+
+        public int Size() => (Top < 0) ? 0 : Top + 1;
+
+        public bool isEmptyStack() => Top < 0;
+
+        public bool isFullStack() => Top < Capacity - 1;
+
+        public void PrintStack()
+        {
+            for (int i = 0; i < Capacity; i++)
+                Console.Write($"{arr[i]} || ");
+        }
+    }
+}
---
+++ Stack/StartingPoint.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class StartingPoint
+    {
+        static void Main(string[] args)
+        {
+            Stack sOb = new Stack(10);
+            for (int i = 10; i < 20; i++)
+                sOb.Push(i);
+            
+            Console.WriteLine($"Size of current Stack is : {sOb.Size()}");
+            Console.WriteLine($"Top Element(Peek) in Stack returned : {sOb.Peek()}");
+            Console.WriteLine($"Is Stack Full : {sOb.isFullStack()}");
+            Console.WriteLine($"Is Stack Empty : {sOb.isEmptyStack()}");
+            Console.WriteLine("Printing Stack from Bottom To Top\n");
+            sOb.PrintStack();
+
+            Console.ReadKey();
+        }
+    }
+}
